31a2a72008451753baa374e554613cf8831e62db
forest-fae <surreal.autumn@gmail.com>
2025-01-14 01:08:40 +0300

    feat: continue arrays task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -142,8 +142,9 @@ function getAverage(arr) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  if (arr.length === 0) return true; // If the array is empty, return true
+  return arr.every((str) => str.length === arr[0].length);
 }
 
 /**
@@ -157,8 +158,8 @@ function isSameLength(/* arr */) {
  *    isValueEqualsIndex([2, 1, 0, 4, 5]) => true
  *    isValueEqualsIndex([10, 20, 30, 40, 50]) => false
  */
-function isValueEqualsIndex(/* arr */) {
-  throw new Error('Not implemented');
+function isValueEqualsIndex(arr) {
+  return arr.some((el, index) => el === index);
 }
 
 /**
